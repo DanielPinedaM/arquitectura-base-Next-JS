01723353012ff1dbd32f9b689c34d25d193ff4f5
DanielPinedaM <daniel.pinedamo@amigo.edu.co>
2026-07-08 22:18:30 -0500

    ⚰️ remove(menu): eliminar tooltip de PrimeReact en MenuButton
    - Quitar la importación dinámica y el uso de PrimeReactTooltip, componente que dependía de prime-react.

--- src/shared/ui/menu/MenuButton.tsx
+++ src/shared/ui/menu/MenuButton.tsx
@@ -4,19 +4,12 @@ import { FaArrowLeft } from 'react-icons/fa';
 import dynamic from 'next/dynamic';
 import { useMenuStore } from '@/shared/ui/menu/store/menu.store';
 import Button from '@/shared/ui/buttons/Button';
-const PrimeReactTooltip = dynamic(() => import('@/shared/ui/prime-react/Tooltip'));
 
 export default function MenuButton() {
   const { handleMenu, showMenu } = useMenuStore();
 
   return (
     <>
-      <PrimeReactTooltip
-        target={'tooltip-menu'}
-        content={showMenu ? 'Ocultar' : 'Mostrar'}
-        position='right'
-      />
-
       <span className='tooltip-menu'>
         <Button variant='link' modifiers={['icon-only']} onClick={handleMenu}>
           {showMenu ? <FaArrowLeft className='text-xl' /> : <CiMenuBurger className='text-xl' />}
