c17ddd28d7af021cab8f38efa3060a1997e7d53d
DanielPinedaM <daniel.pinedamo@amigo.edu.co>
2026-07-14 08:32:17 -0500

    💄 style(loader): quitar sombra del overlay del loader fijo
    - Eliminar la clase shadow-lg del contenedor del loader fijo.

--- src/shared/ui/loader/FixedLoader.tsx
+++ src/shared/ui/loader/FixedLoader.tsx
@@ -2,7 +2,7 @@ import { RiLoader4Fill } from 'react-icons/ri';
 
 export default function FixedLoader() {
   return (
-    <div className='fixed inset-0 z-999 bg-[oklch(100%_0_0/0.18)] backdrop-blur-[1px] backdrop-saturate-150 shadow-lg w-dvw h-dvh flex justify-center items-center cursor-wait'>
+    <div className='fixed inset-0 z-999 bg-[oklch(100%_0_0/0.18)] backdrop-blur-[1px] backdrop-saturate-150 w-dvw h-dvh flex justify-center items-center cursor-wait'>
       <RiLoader4Fill className='animate-spin text-blue-500 text-7xl' />
     </div>
   );
